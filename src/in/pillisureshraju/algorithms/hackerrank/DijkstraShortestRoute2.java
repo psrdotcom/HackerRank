a39bf9d5bd651fdd11c2918ad352fb5cde4265ef
Suresh Raju Pilli <sureshraju.pilli@gmail.com>
2017-05-02 00:27:23 +0530

    Added print distance matrix

--- src/in/pillisureshraju/algorithms/hackerrank/DijkstraShortestRoute2.java
+++ src/in/pillisureshraju/algorithms/hackerrank/DijkstraShortestRoute2.java
@@ -4,42 +4,67 @@ import java.util.Scanner;
 
 public class DijkstraShortestRoute2 {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        int weight_matrix[][] = null;
-        for(int a0 = 0; a0 < t; a0++){
-            int n = in.nextInt(); // Vertices
-            int m = in.nextInt(); // Edges
-            weight_matrix = new int[n][n];
-            for(int a1 = 0; a1 < m; a1++){
-                int x = in.nextInt(); // Source Edge
-                int y = in.nextInt(); // Destination Edge
-                int r = in.nextInt(); // Weight
-                weight_matrix[x-1][y-1] = r;
-            }
-            int s = in.nextInt(); // Starting Edge
-            //printWeigthMatrix(weight_matrix);
-            // Get the direct distances of the selected source
-        }
-        
-        
-        in.close();
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		int weight_matrix[][] = null;
+		for (int a0 = 0; a0 < t; a0++) {
+			int n = in.nextInt(); // Vertices
+			int m = in.nextInt(); // Edges
+			weight_matrix = new int[n][n];
+			for (int a1 = 0; a1 < m; a1++) {
+				int x = in.nextInt(); // Source Edge
+				int y = in.nextInt(); // Destination Edge
+				int r = in.nextInt(); // Weight
+				weight_matrix[x - 1][y - 1] = r;
+			}
+			int s = in.nextInt(); // Starting Edge
+			// Validate starting edge
+			if (s > n) {
+				System.out.println("Invalid starting edge");
+				continue;
+			} else {
+				s--; // To match with array representation
+			}
+
+			printWeigthMatrix(weight_matrix);
+
+			// Get the direct distances of the selected source
+			int dist[] = new int[n];
+			for (int i = 0; i < weight_matrix[s].length; i++) {
+				dist[i] = weight_matrix[s][i];
+			}
+
+			printDistancesFromSource(dist);
+		}
+
+		in.close();
+	}
 
 	/**
+	 * Print the distances from source
+	 * 
+	 * @param dist
+	 */
+	private static void printDistancesFromSource(int[] dist) {
+		System.out.println("Distance from source: ");
+		for (int i : dist) {
+			System.out.println(i + " ");
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Print the weight matrix
+	 * 
 	 * @param weight_matrix
 	 */
 	private static void printWeigthMatrix(int[][] weight_matrix) {
-		for(int i = 0; i < weight_matrix.length; i++ ) {
-        	for (int j = 0; j < weight_matrix[i].length; j++ ) {
-        		System.out.print(weight_matrix[i][j] + " ");
-        	}
-        	System.out.println();
-        }
+		for (int i = 0; i < weight_matrix.length; i++) {
+			for (int j = 0; j < weight_matrix[i].length; j++) {
+				System.out.print(weight_matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
-}
-
-class WeigthedGraph {
-	
 }
